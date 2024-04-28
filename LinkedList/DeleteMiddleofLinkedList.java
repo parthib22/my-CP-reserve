0fc76f92f3871f09a2144193b536eef07143dd69
Parthib DS <parthib22.ds@gmail.com>
2024-04-28 17:07:01 +0530

    java & gitignore

---
+++ LinkedList/DeleteMiddleofLinkedList.java
@@ -0,0 +1,62 @@
+package LinkedList;
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+
+}
+
+class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Node head = new Node(sc.nextInt());
+        Node tail = head;
+        for (int i = 0; i < n - 1; i++) {
+            tail.next = new Node(sc.nextInt());
+            tail = tail.next;
+        }
+        Solution g = new Solution();
+        head = g.deleteMid(head);
+        printList(head);
+        sc.close();
+    }
+}
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node fast = head;
+        Node slow = head;
+        Node temp = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            temp = slow;
+            slow = slow.next;
+        }
+
+        temp.next = temp.next.next;
+
+        return head;
+    }
+}
